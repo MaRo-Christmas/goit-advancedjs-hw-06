d762bdeee39c18faa83e9fc8df5cf7c38976e872
Maryna_Rozhdestvenska <rozhd.buro@gmail.com>
2025-01-20 04:50:52 +0200

    modified:   src/baseTypes/1.ts 	modified:   src/baseTypes/2.ts 	modified:   src/baseTypes/3.ts 	modified:   src/baseTypes/4.ts 	modified:   src/baseTypes/5.ts 	modified:   src/baseTypes/6.ts 	modified:   src/baseTypes/7.ts 	modified:   src/baseTypes/8.ts 	modified:   src/baseTypes/9.ts 	modified:   src/generic/1.ts 	modified:   src/generic/2.ts 	modified:   src/generic/3.ts 	modified:   src/generic/4.ts 	modified:   src/generic/5.ts 	modified:   src/generic/6.ts 	modified:   src/generic/7.ts 	modified:   src/generic/8.ts

--- src/baseTypes/7.ts
+++ src/baseTypes/7.ts
@@ -2,3 +2,20 @@
   Створіть функцію (isWeekend), яка приймає день тижня (з вашого enum)
   і повертає boolean значення, що вказує, чи це день робочий чи вихідний.
 */
+
+enum WeekDays {
+  Monday = "Monday",
+  Tuesday = "Tuesday",
+  Wednesday = "Wednesday",
+  Thursday = "Thursday",
+  Friday = "Friday",
+  Saturday = "Saturday",
+  Sunday = "Sunday",
+}
+
+function isWeekend(day: string): boolean {
+  return day === WeekDays.Saturday || day === WeekDays.Sunday;
+}
+
+console.log(isWeekend(WeekDays.Monday));
+console.log(isWeekend(WeekDays.Saturday));
--- src/generic/5.ts
+++ src/generic/5.ts
@@ -3,9 +3,27 @@
   Використовуйте generics, щоб цей інтерфейс міг працювати з будь-якими типами ключів та значень.
 */
 
-interface KeyValuePair {
-  key;
-  value;
+interface KeyValuePair<K, V> {
+  key: K; // Тип ключа
+  value: V; // Тип значення
 }
+
+// Пара ключ-значення: рядок і число
+const pair1: KeyValuePair<string, number> = {
+  key: "age",
+  value: 30,
+};
+
+// Пара ключ-значення: число і булевий тип
+const pair2: KeyValuePair<number, boolean> = {
+  key: 1,
+  value: true,
+};
+
+// Пара ключ-значення: об'єкт і масив рядків
+const pair3: KeyValuePair<{ id: number }, string[]> = {
+  key: { id: 42 },
+  value: ["Alice", "Bob"],
+};
 
 export {};
